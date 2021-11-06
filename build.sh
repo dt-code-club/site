f049c15c44212a9cb911b525bf78b149a2e22b42
climbTheStairs <yxtqwf+github@gmail.com>
2021-11-06 19:14:49 +0000

    :truck: Merge gentree.sh into build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Dependencies:
-# - Pandoc
+# - pandoc
+# - tree
 
 [ "$(basename "$0")" = 'rebuild.sh' ] && rebuild=1
 
@@ -21,10 +22,29 @@ gen_html() {
 
     echo "Generating $html..."
     mkdir --parents "$(dirname "$html")"
-    pandoc "$md" --output="$html" --template='etc/template.html' --no-highlight
+    pandoc --output="$html" --template='etc/template.html' --no-highlight -- "$md"
 }
+
+gen_sitemap() {
+    echo 'Generating srv/sitemap.md...'
+    {
+        echo '---'
+        echo 'title: Sitemap'
+        echo '---'
+        echo
+        echo '```'
+        echo '.'
+        tree --charset='ascii' srv | tail --lines='+2'
+        echo '```'
+    } > srv/sitemap.md
+    gen_html 'srv/sitemap'
+}
+
+rm srv/sitemap.md
 
 # WARNING: Will not work on file names containing newlines!
 find srv -name '*.md' | while read -r f; do
     gen_html "${f%.md}"
 done
+
+gen_sitemap
