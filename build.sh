698a6c28c15972a86275b713f4b2aacfbcafc1b6
climbTheStairs <yxtqwf+github@gmail.com>
2021-11-08 16:12:35 +0000

    :sparkles: Add `$root_dir` variable, new `write_abs_links()` into build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 # - pandoc
 # - tree
 
+root_dir=srv
+
 [ "$(basename "$0")" = 'rebuild.sh' ] && rebuild=1
 
 gen_html() {
@@ -26,8 +28,8 @@ gen_html() {
 }
 
 gen_sitemap() {
-    echo 'Generating srv/sitemap/index.html...'
-    mkdir --parents 'srv/sitemap'
+    echo "Generating $root_dir/sitemap/index.html..."
+    mkdir --parents "$root_dir/sitemap"
     {
         echo '---'
         echo 'title: Sitemap'
@@ -35,14 +37,22 @@ gen_sitemap() {
         echo
         echo '```'
         echo '.'
-        tree --charset='ascii' srv | tail --lines='+2'
+        tree --charset='ascii' "$root_dir" | tail --lines='+2'
         echo '```'
-    } | pandoc --output='srv/sitemap/index.html' --defaults='etc/html.yaml'
+    } | pandoc --output="$root_dir/sitemap/index.html" --defaults='etc/html.yaml'
+}
+
+write_abs_links() {
+    dir=$(echo "${1#$root_dir}/" | sed 's/\//\\\//g')
+    rel_link_script="s/\(\[.\+\](\)\([a-z0-9][\/a-z0-9.-]\+)\)/\1$dir\2/g"
+    root_link_script="s/\(\[.\+\](\)\(\/[a-z0-9][\/a-z0-9.-]\+)\)/\1https:\/\/dtcode.club\2/g"
+    sed "$rel_link_script; $root_link_script" "$f"
 }
 
 # WARNING: Will not work on file names containing newlines!
-find srv -name '*.md' | while read -r f; do
+find "$root_dir" -name '*.md' | while read -r f; do
     gen_html "${f%.md}"
+    # write_abs_links "${f%.md}"
 done
 
 gen_sitemap
