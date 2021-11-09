634bd5b5c7c0092bf0eb7953a7fdd0e7f001e3fd
climbTheStairs <yxtqwf+github@gmail.com>
2021-11-09 09:48:05 +0000

    :plunger: Clean up build.sh
    Annotate with comments, break long lines, and rename variables

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 root_dir='srv'
 domain='dtcode.club'
 
-[ "$1" = '-r' ] && rebuild=1
+[ "$1" = '-f' ] && force_build=1
 
 gen_html() {
     md="$1.md"
@@ -18,7 +18,10 @@ gen_html() {
         html="$1/index.html"
     fi
 
-    if [ ! "$rebuild" ] && [ -f "$html" ]; then
+    # If the HTML file exists
+    # and is newer than both the markdown source and the template,
+    # do not build, unless it has been forced with `-f`.
+    if [ ! "$force_build" ] && [ -f "$html" ]; then
         [ -n "$(find "$md"               -newer "$html")" ] ||
         [ -n "$(find 'etc/template.html' -newer "$html")" ] ||
         return
@@ -49,12 +52,25 @@ write_abs_links() {
 
     echo "Generating $pdf..."
 
-    dir=$(echo "${1#$root_dir}/" | sed 's/\//\\\//g')
-    rel_link_script="s/\(\[.\+\](\)\([a-z0-9][\/a-z0-9.-]\+)\)/\1$dir\2/g"
-    root_link_script="s/\(\[.\+\](\)\(\/[a-z0-9][\/a-z0-9.-]\+)\)/\1https:\/\/$domain\2/g"
+    # Remove leading `$root_dir` and escape `/` with `\/`
+    root_dir=$(echo "${1#$root_dir}/" | sed 's/\//\\\//g')
+    # Make relative links (`[text](relative/link)`)
+    # into absolute root links (`[text](/root/link)`)
+    rel_link_script="s"\
+        "/\(\[.\+\](\)\([a-z0-9][\/a-z0-9.-]\+)\)"\
+        "/\1$root_dir\2/g"
+    # Make absolute domain links (see above)
+    # into full links (`[text](https://full/link)`)
+    root_link_script="s"\
+        "/\(\[.\+\](\)\(\/[a-z0-9][\/a-z0-9.-]\+)\)"\
+        "/\1https:\/\/$domain\2/g"
+    # File names *must* be lowercase
+    # and start with a letter or number,
+    # and they may only contain letters, numbers, hyphens, and dots
     sed "$rel_link_script; $root_link_script" "$md"
 }
 
+# Remove sitemap.md to prevent it from being built twice
 rm -- "$root_dir/sitemap.md"
 # WARNING: Will not work on file names containing newlines!
 find "$root_dir" -name '*.md' | while read -r f; do
