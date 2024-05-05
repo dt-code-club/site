523f8b1da34ea2b389d9c6d350f571709666d766
AW_Dev <riptidegamesceo@gmail.com>
2024-05-04 21:23:20 -0700

    added feature for command history

--- script/script.js
+++ script/script.js
@@ -4,8 +4,16 @@ const dtccHelpText = document.getElementById("dtcc-help").innerText
 const osHelpText = document.getElementById("os-help").innerText
 const loaderContainer = document.querySelector(".os-loader-container")
 const caret = "▍"
+const ignoreKeys = [
+    'Shift', 'Control', 'Alt', 'Meta',
+    /*'ArrowUp', 'ArrowDown',*/ 'ArrowLeft', 'ArrowRight',
+    'Tab', 'Escape',
+    'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'
+];
+const dev = false;
 var inputBuffer = "";
 var commandHistory = [];
+var commandIndex = 0;
 async function typeLine(line, typeSpeed = 35, end = "\n") {
     return new Promise((resolve, reject) => {
         line += end
@@ -36,7 +44,21 @@ async function input(prompt) {
             } else if (key == "Backspace") {
                 inputBuffer = inputBuffer.substring(0, inputBuffer.length - 1);
                 terminal.innerText = startText + inputBuffer + caret
-            } else if (key == "Shift" || key == "Alt" || key == "Control" || key == "Meta") {
+            } else if (key == "ArrowUp") {
+                if (commandIndex > 0) {
+                    commandIndex--;
+                    let newCommand = commandHistory[commandIndex]
+                    inputBuffer = newCommand
+                    terminal.innerText = startText + inputBuffer + caret
+                }
+            } else if (key == "ArrowDown") {
+                if (commandIndex < commandHistory.length - 1) {
+                    commandIndex++;
+                    let newCommand = commandHistory[commandIndex]
+                    inputBuffer = newCommand
+                    terminal.innerText = startText + inputBuffer + caret
+                }
+            } else if (ignoreKeys.includes(key)) {
 
             }
             else {
@@ -53,18 +75,18 @@ async function sleepAsync(time) {
 }
 async function init() {
     canUserType = false;
-    await typeLine(`Incoming transmission - ${transmissionText.length} bytes`, 20)
-    await sleepAsync(200)
-    await typeLine("Begin transmission...", 20)
-    await typeLine("---------------------------------------------------------------------------", 10)
-    await sleepAsync(200)
+    await typeLine(`Incoming transmission - ${transmissionText.length} bytes`, dev ? 0 : 20)
+    await sleepAsync(dev ? 0 : 200)
+    await typeLine("Begin transmission...", dev ? 0 : 20)
+    await typeLine("---------------------------------------------------------------------------", dev ? 0 : 10)
+    await sleepAsync(dev ? 0 : 200)
     for (const line of transmissionText.split("\n")) {
-        await typeLine(line, 15);
+        await typeLine(line, dev ? 0 : 15);
     };
-    await sleepAsync(200)
-    await typeLine("---------------------------------------------------------------------------", 10)
-    await typeLine("End transmission.\n", 20)
-    await sleepAsync(200)
+    await sleepAsync(dev ? 0 : 200)
+    await typeLine("---------------------------------------------------------------------------", dev ? 0 : 10)
+    await typeLine("End transmission.\n", dev ? 0 : 20)
+    await sleepAsync(dev ? 0 : 200)
     canUserType = true;
     while (true) {
         let result = await input("visitor@codeclub.local:~$ ")
@@ -75,27 +97,27 @@ async function init() {
         if (command == "dtcc") {
             if (arguments[0] == "help" || arguments[0] == "hp") {
                 for (const line of dtccHelpText.split("\n")) {
-                    await typeLine(line, 2);
+                    await typeLine(line, dev ? 0 : 2);
                 };
             } else {
                 await typeLine("Error: unrecognized or incomplete command line.\n", 3)
                 for (const line of dtccHelpText.split("\n")) {
-                    await typeLine(line, 2);
+                    await typeLine(line, dev ? 0 : 2);
                 };
             }
         } else if (command == "os") {
             if (arguments[0] == "activate" || arguments[0] == "ac") {
-                await typeLine("Activating operating system GUI...", 10);
-                await sleepAsync(750);
+                await typeLine("Activating operating system GUI...", dev ? 0 : 10);
+                await sleepAsync(dev ? 0 : 750);
                 loaderContainer.style.display = "initial"
             } else if (arguments[0] == "help" || arguments[0] == "hp") {
                 for (const line of osHelpText.split("\n")) {
-                    await typeLine(line, 2);
+                    await typeLine(line, dev ? 0 : 2);
                 };
             } else {
                 await typeLine("Error: unrecognized or incomplete command line.\n", 3)
                 for (const line of osHelpText.split("\n")) {
-                    await typeLine(line, 2);
+                    await typeLine(line, dev ? 0 : 2);
                 };
             }
         } else if (command == "echo") {
@@ -104,6 +126,7 @@ async function init() {
             await typeLine(`"${command}" is not recognized as an internal or external command.\n`, 3);
         }
         console.log(commandHistory)
+        commandIndex++;
     }
 }
 init()
