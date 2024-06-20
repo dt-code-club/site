2fea83a173d2ab0ffcf933f01d38dc02b9c909c2
= <riptidegamesceo@gmail.com>
2024-06-20 10:00:07 -0700

    i left dev mode on again

--- script/script.js
+++ script/script.js
@@ -13,7 +13,7 @@ const ignoreKeys = [
     'Tab', 'Escape',
     'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'
 ];
-const dev = true;
+const dev = false;
 var inputBuffer = "";
 var commandHistory = [];
 var commandIndex = 0;
